a0de3065e3f9230f9eefca63235efaa0f0a24cf9
chrisgzj <chrisgzj@gmail.com>
2020-12-02 09:15:25 -0600

    Added solution for december the 2nd.

--- DailyChallengeDec2020.py
+++ DailyChallengeDec2020.py
@@ -8,7 +8,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
+class Solution_dec1(object):
     def recursiveDepth(self, current_node, count):
         """For the current node, if it is not None, updates de counter variable and calls itself for the left and right nodes"""
         if current_node is None:
@@ -21,4 +21,44 @@ class Solution(object):
         """
         return self.recursiveDepth(root, 0)
 
-    
+
+
+# December, 2nd. Definition for singly-linked list.
+# Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import random
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        Creates an instance of the class and counts the number of elements in the list.
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+        current_node = head
+        counter = 0
+        while current_node is not None:
+            counter += 1
+            current_node = current_node.next
+        self.counter = counter
+        self.head = head
+
+    def getRandom(self):
+        """
+        Takes a random number and returns the value in that element of the list
+        Returns a random node's value.
+        :rtype: int
+        """
+        random_index = random.randrange(self.counter)
+        current_node = self.head
+        for i in range(random_index):
+            current_node = current_node.next
+        return current_node.val
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()

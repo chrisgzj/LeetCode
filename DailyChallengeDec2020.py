f40b13afb5be0963af29225716412886d362a262
chrisgzj <chrisgzj@gmail.com>
2020-12-10 13:24:18 -0600

    Added solution for december 10.

--- DailyChallengeDec2020.py
+++ DailyChallengeDec2020.py
@@ -416,10 +416,41 @@ class BSTIterator(object):
         """
         return self.counter < self.total_length
 
-test_root = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6, None, TreeNode(8, TreeNode(7), TreeNode(9))))
+# test_root = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6, None, TreeNode(8, TreeNode(7), TreeNode(9))))
+# test_iter = BSTIterator(test_root)
+# print(test_iter.next())
+# print(test_iter.hasNext())
 
-test_iter = BSTIterator(test_root)
+#--------------------------------------------------------
+# December, 10th. Valid Mountain Array
+# Given an array of integers arr, return true if and only if it is a valid mountain array.
+# Recall that arr is a mountain array if and only if:
+# arr.length >= 3
+# There exists some i with 0 < i < arr.length - 1 such that:
+# arr[0] < arr[1] < ... < arr[i - 1] < A[i]
+# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
-print(test_iter.next())
-print(test_iter.hasNext())
+class Solution_10(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        if len(arr) < 3:
+            return False
+        if arr[0] >= arr[1]:
+            return False
+        reached_peak = False
+        for index in range(1, len(arr)):
+            if not reached_peak:
+                if arr[index] == arr[index-1]:
+                    return False
+                if arr[index] < arr[index-1]:
+                    reached_peak = True
+            else:
+                if arr[index] >= arr[index-1]:
+                    return False
+        return reached_peak
 
+sol = Solution_10()
+print(sol.validMountainArray([1,3,2]))

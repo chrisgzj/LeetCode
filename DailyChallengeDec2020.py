4556f3b3528e0961e6196303fd708113c229ef81
chrisgzj <chrisgzj@gmail.com>
2020-12-11 18:33:11 -0600

    Added solution for december 11.

--- DailyChallengeDec2020.py
+++ DailyChallengeDec2020.py
@@ -452,5 +452,26 @@ class Solution_10(object):
                     return False
         return reached_peak
 
-sol = Solution_10()
-print(sol.validMountainArray([1,3,2]))
+# sol = Solution_10()
+# print(sol.validMountainArray([1,3,2]))
+
+#--------------------------------------------------------
+# December, 11th. Remove Duplicates from Sorted Array II
+# Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+# Do not allocate extra space for another array; you must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        Function that iterates over the array deleting the elemnts repeated more than twice.
+        """
+        if len(nums) >= 3:
+            index = 2
+            while index < len(nums):
+                if nums[index] == nums[index - 2]:
+                    del nums[index]
+                else:
+                    index += 1 # Only increases when element is not deleted.
+        return len(nums)
